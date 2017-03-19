2b5512a8d5ba2cfb9ec3021f5a2729a95cdae088
Mattin <mattinzargarpur@gmail.com>
2017-03-19 15:55:50 -0400

    Added image files to data directory.

---
+++ VR4GETAR/Assets/Scripts/TEST_Image.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TEST_Image : MonoBehaviour
+{
+	// Use this for initialization
+	void Start ()
+    {
+        GetComponent<Renderer>().material.mainTexture = imageToTexture("Assets/Data/testImage.jpg");
+		
+	}
+
+    private Texture2D imageToTexture(string path)
+    {
+        Texture2D texture = new Texture2D(2, 2);
+        byte[] imageData = System.IO.File.ReadAllBytes(path);
+        texture.LoadImage(imageData);
+
+        return texture;
+    }
+}
